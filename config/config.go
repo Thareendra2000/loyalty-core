d51368c676c98fae867250e5428f307b3f793259
Thareendra2000 <thareendra2000@gmail.com>
2025-07-06 22:06:52 +0530

    Add Square integration for loyalty program

--- config/config.go
+++ config/config.go
@@ -1,29 +1,38 @@
 package config
 
 import (
-    "os"
-    "github.com/joho/godotenv"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-    Port      string
-    JWTSecret string
+	Port                string
+	JWTSecret           string
+	SquareAccessToken   string
+	SquareApplicationID string
+	SquareLocationID    string
+	SquareEnvironment   string
 }
 
 func LoadConfig() (*Config, error) {
-    godotenv.Load()
-    
-    config := &Config{
-        Port:      getEnv("PORT", "8080"),
-        JWTSecret: getEnv("JWT_SECRET", "your-secret-key"),
-    }
-    
-    return config, nil
+	godotenv.Load()
+
+	config := &Config{
+		Port:                getEnv("PORT", "8080"),
+		JWTSecret:           getEnv("JWT_SECRET", "your-secret-key"),
+		SquareAccessToken:   getEnv("SQUARE_ACCESS_TOKEN", "your-square-access-token"),
+		SquareApplicationID: getEnv("SQUARE_APPLICATION_ID", "your-square-application-id"),
+		SquareLocationID:    getEnv("SQUARE_LOCATION_ID", "your-square-location-id"),
+		SquareEnvironment:   getEnv("SQUARE_ENVIRONMENT", "sandbox"),
+	}
+
+	return config, nil
 }
 
 func getEnv(key, defaultValue string) string {
-    if value := os.Getenv(key); value != "" {
-        return value
-    }
-    return defaultValue
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
 }
